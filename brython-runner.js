d52a40db432bfd3658a3c37052abc70753fcb156
Albert-7317 <103575236+Albert-7317@users.noreply.github.com>
2022-05-06 14:04:06 +0100

    Add files via upload

---
+++ brython-runner.js
@@ -0,0 +1,13 @@
+"use strict";
+
+var _interopRequireDefault = ("@babel/runtime/helpers/interopRequireDefault");
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports["default"] = void 0;
+
+var _brythonRunner = _interopRequireDefault(require("H:\Code\Brython\brython-runner\lib\core\brython-runner.js"));
+
+var _default = _brythonRunner["default"];
+exports["default"] = _default;
